7228d30a88345582f86620498df1c91c07271bcc
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-11-16 14:51:16 -0500

    Updated commands for bpti convserINO

--- src/commands.sh
+++ src/commands.sh
@@ -22,9 +22,14 @@ t.save("./Trajectories/trj0.h5")
 RebuildProject -c native.pdb
 
 # Work on 10ns subsampled dataset for now, uses less memory.
+# For BPTI, will do full dataset, but note this uses LOTS of memory.
 Cluster rmsd hierarchical
 AssignHierarchical -s 1 -n 10
 
+# Build transition matrix, do ergodic trimming etc.
+BuildMSM -l 1
+
+#Output some structures for visualization.
 SaveStructures -c 3 -S sep -f pdb
 
 pymol PDBs/*.pdb
--- src/convert.py
+++ src/convert.py
@@ -5,3 +5,25 @@ filename = "/home/kyleb/dat/ShawScience/TarBalls/DESRES-Trajectory_2F4K-0-protei
 traj = md.load([filename % i for i in range(62)], top="protein.pdb", stride=5)
 
 traj.save("./Trajectories/trj0.h5")
+
+
+import mdtraj as md
+import numpy as np
+
+# First use VMD to save 2F4K-0-protein.mae as protein.pdb 
+filename = "/home/kyleb/dat/ShawScience/TarBalls/DESRES-Trajectory-bpti-100/bpti-100/bpti-100-%.3d.dcd"
+trj0 = md.load("system.pdb")
+atom_indices = np.arange(892)  # Just protein
+
+traj = md.load([filename % i for i in range(42)], top=trj0, stride=1, atom_indices=atom_indices)
+trj0.atom_slice(atom_indices, inplace=True)
+
+traj.center_coordinates()
+traj.superpose(trj0)
+
+traj.save("./Trajectories/trj0.h5")
+
+
+trj0.save("./native.pdb")
+
+traj.save("./out.dcd")
